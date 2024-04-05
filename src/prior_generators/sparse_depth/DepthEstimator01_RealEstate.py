59b0a3769e86e4571b8c86a466345ad8ac2c7702
yiji cheng <yiji_cheng@outlook.com>
2024-04-05 13:25:23 +0000

    add depth

--- src/prior_generators/sparse_depth/DepthEstimator01_RealEstate.py
+++ src/prior_generators/sparse_depth/DepthEstimator01_RealEstate.py
@@ -9,11 +9,17 @@ import time
 import traceback
 from pathlib import Path
 
+from io import BytesIO
+from PIL import Image
+
+import torch
 import numpy
 import pandas
 import simplejson
 import skimage.io
 from deepdiff import DeepDiff
+from tqdm import tqdm
+from collections import defaultdict
 from tqdm import tqdm
 
 import Tester01 as Tester
@@ -41,45 +47,71 @@ def save_configs(output_dirpath: Path, configs: dict):
         simplejson.dump(configs, configs_file, indent=4)
     return
 
+def convert_images(
+    images,
+): # -> Float[Tensor, "batch 3 height width"]:
+    numpy_images = []
+    for image in images:
+        image = Image.open(BytesIO(image.numpy().tobytes()))
+        numpy_images.append(numpy.array(image))
+    return numpy.stack(numpy_images)
 
-def start_generation(gen_configs: dict):
-    root_dirpath = Path('../../../')
-    database_dirpath = root_dirpath / 'data/databases' / gen_configs['database_dirpath']
-    tmp_dirpath = root_dirpath / 'tmp'
+def start_generation(split, chunk_s=None, chunk_e=None):
+    # root_dirpath = Path('../../../') # root
+    root_dirpath = Path('./data') # root
+    database_dirpath = root_dirpath / "RealEstate10K/re10k" # data
+    tmp_dirpath = root_dirpath / 'tmp' # tmp
 
-    output_dirpath = database_dirpath / f"test/estimated_depths/DE{gen_configs['gen_num']:02}"
+    output_dirpath = database_dirpath / "estimated_depths/" / split # out
     output_dirpath.mkdir(parents=True, exist_ok=True)
-    save_configs(output_dirpath, gen_configs)
 
-    set_num = gen_configs['gen_set_num']
-    video_datapath = database_dirpath / f'train_test_sets/set{set_num:02}/TrainVideosData.csv'
-    video_data = pandas.read_csv(video_datapath)
-    scene_nums = numpy.unique(video_data['scene_num'].to_numpy())
+
+    scene_to_chunk_json = database_dirpath / split / "index.json"
+    scene_to_chunk = json.load(open(scene_to_chunk_json, "r"))
+
+    chunk_to_scene = defaultdict(list)
+    for scene, chunk in scene_to_chunk.items():
+        chunk_to_scene[chunk].append(scene)
 
     tester = Tester.ColmapTester(tmp_dirpath)
 
-    for scene_num in tqdm(scene_nums):
-        bounds_path = output_dirpath / f'{scene_num:05}/EstimatedBounds.csv'
-        if bounds_path.exists():
+
+    # for scene_num in tqdm(scene_nums):
+    for chunk in tqdm(sorted(chunk_to_scene.keys())):
+        chunk_block = torch.load(database_dirpath / split / chunk)
+        depth_chunk = {}
+        depth_chunk_path = output_dirpath / chunk
+        if depth_chunk_path.exists():
             continue
+        for scene in chunk_block:
 
-        frame_nums = video_data.loc[video_data['scene_num'] == scene_num]['pred_frame_num'].to_numpy()
-        frames = [read_image(database_dirpath / f'test/database_data/{scene_num:05}/rgb/{frame_num:04}.png') for frame_num in frame_nums]
-        frames = numpy.stack(frames)
-        intrinsics_path = database_dirpath / f'test/database_data/{scene_num:05}/CameraIntrinsics.csv'
-        extrinsics_path = database_dirpath / f'test/database_data/{scene_num:05}/CameraExtrinsics.csv'
-        intrinsics = numpy.loadtxt(intrinsics_path.as_posix(), delimiter=',').reshape((-1, 3, 3))[frame_nums]
-        extrinsics = numpy.loadtxt(extrinsics_path.as_posix(), delimiter=',').reshape((-1, 4, 4))[frame_nums]
 
-        depth_data_list, bounds_data = tester.estimate_sparse_depth(frames, extrinsics, intrinsics)
-        if depth_data_list is None:
-            continue
+            frames = convert_images(scene["images"]) # 0-255 # (98, 360, 640, 3)
+            h, w = frames.shape[1], frames.shape[2]
 
-        for i, frame_num in enumerate(frame_nums):
-            depth_path = output_dirpath / f'{scene_num:05}/estimated_depths/{frame_num:04}.csv'
-            depth_path.parent.mkdir(parents=True, exist_ok=True)
-            depth_data_list[i].to_csv(depth_path, index=False)
-        bounds_data.to_csv(bounds_path, index=False)
+            intrinsics = numpy.zeros((frames.shape[0], 3, 3))
+            intrinsics[:, 2, 2] = 1
+            intrinsics[:, 0, 0] = scene["cameras"][:, 0] * w # fx
+            intrinsics[:, 1, 1] = scene["cameras"][:, 1] * h # fy
+            intrinsics[:, 0, 2] = scene["cameras"][:, 2] * w # cx
+            intrinsics[:, 1, 2] = scene["cameras"][:, 3] * h # cy
+
+
+            w2c_mat = numpy.array(scene["cameras"][:, 6:]).reshape(-1, 3, 4)
+            extrinsics = numpy.zeros((frames.shape[0], 4, 4))
+            extrinsics[:, 3, 3] = 1
+            extrinsics[:, :3, :] = w2c_mat
+            
+
+            depth_data_list, bounds_data = tester.estimate_sparse_depth(frames, extrinsics, intrinsics)
+            if depth_data_list is None:
+                continue
+            
+            depth_chunk[scene["key"]] = {"depth": depth_data_list, "bounds": bounds_data}
+
+        torch.save(depth_chunk, depth_chunk_path)
+        import pdb; pdb.set_trace()
+            
     return
 
 
@@ -88,32 +120,26 @@ def demo1():
     For a gen set
     :return:
     """
-    gen_configs = {
-        'generator': this_filename,
-        'gen_num': 2,
-        'gen_set_num': 2,
-        'database_name': 'RealEstate10K',
-        'database_dirpath': 'RealEstate10K/data',
-    }
-    start_generation(gen_configs)
 
-    gen_configs = {
-        'generator': this_filename,
-        'gen_num': 3,
-        'gen_set_num': 3,
-        'database_name': 'RealEstate10K',
-        'database_dirpath': 'RealEstate10K/data',
-    }
-    start_generation(gen_configs)
+    start_generation("test")
 
-    gen_configs = {
-        'generator': this_filename,
-        'gen_num': 4,
-        'gen_set_num': 4,
-        'database_name': 'RealEstate10K',
-        'database_dirpath': 'RealEstate10K/data',
-    }
-    start_generation(gen_configs)
+    # gen_configs = {
+    #     'generator': this_filename,
+    #     'gen_num': 3,
+    #     'gen_set_num': 3,
+    #     'database_name': 'RealEstate10K',
+    #     'database_dirpath': 'RealEstate10K/data',
+    # }
+    # start_generation(gen_configs)
+
+    # gen_configs = {
+    #     'generator': this_filename,
+    #     'gen_num': 4,
+    #     'gen_set_num': 4,
+    #     'database_name': 'RealEstate10K',
+    #     'database_dirpath': 'RealEstate10K/data',
+    # }
+    # start_generation(gen_configs)
     return
 
 
@@ -125,13 +151,13 @@ def main():
 if __name__ == '__main__':
     print('Program started at ' + datetime.datetime.now().strftime('%d/%m/%Y %I:%M:%S %p'))
     start_time = time.time()
-    try:
-        main()
-        run_result = 'Program completed successfully!'
-    except Exception as e:
-        print(e)
-        traceback.print_exc()
-        run_result = 'Error: ' + str(e)
-    end_time = time.time()
-    print('Program ended at ' + datetime.datetime.now().strftime('%d/%m/%Y %I:%M:%S %p'))
-    print('Execution time: ' + str(datetime.timedelta(seconds=end_time - start_time)))
+    # try:
+    main()
+    run_result = 'Program completed successfully!'
+    # except Exception as e:
+    #     print(e)
+    #     traceback.print_exc()
+    #     run_result = 'Error: ' + str(e)
+    # end_time = time.time()
+    # print('Program ended at ' + datetime.datetime.now().strftime('%d/%m/%Y %I:%M:%S %p'))
+    # print('Execution time: ' + str(datetime.timedelta(seconds=end_time - start_time)))
